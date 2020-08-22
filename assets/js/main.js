4c1cf6ebae23482e9c0c2fb23a7569afbfa07428
Barış Albayrak <barisalbayrak34@gmail.com>
2020-08-23 02:19:14 +0300

    start #4

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,11 @@ $(function () {
         if ( $(window).scrollTop() > 10 ) {
             $('.navbar').addClass('dx-nav-active');
         } else {
-            $('.navbar').removeClass('dx-nav-active');
-        }
+            currentvalue = document.getElementById("dx_onOff").value;
+            if (currentvalue == "open") {
+            $('.navbar').addClass('dx-nav-active'); } else {
+                $('.navbar').removeClass('dx-nav-active');
+        }}
     });
   });
 
@@ -17,5 +20,33 @@ $(function () {
         } else {
             document.getElementById("scrollTop").style.opacity = "0";
         }
-    });
+ });
   });
+
+  function flipper(x) {
+    x.classList.toggle("flipper");
+  }
+
+  function dx_toggler(x) {
+    flipper(x);
+    currentvalue = document.getElementById("dx_onOff").value;
+    if (currentvalue == "open") {
+    document.getElementById("dx_onOff").value = "undefined";
+    } else {
+        document.getElementById("dx_onOff").value = "open"; 
+    }
+    dx_bridge();
+  }
+
+ function dx_bridge() {
+     currentvalue = document.getElementById("dx_onOff").value;
+     if (currentvalue == "open") {
+        $('.navbar').addClass('dx-nav-active');
+     } else {
+        if ( $(window).scrollTop () > 0 ) {
+        $('.navbar').addClass('dx-nav-active');
+     } else {
+        $('.navbar').removeClass('dx-nav-active');
+        }}
+
+  }
